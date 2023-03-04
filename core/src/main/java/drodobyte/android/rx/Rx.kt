c24671721dab4a9fd32327c9f62c65b678504838
drodobyte <drodobyte@gmail.com>
2023-03-04 15:06:56 +0100

    fix rx

--- core/src/main/java/drodobyte/android/rx/Rx.kt
+++ core/src/main/java/drodobyte/android/rx/Rx.kt
@@ -6,8 +6,8 @@ import io.reactivex.android.schedulers.AndroidSchedulers.mainThread
 
 open class Rx(private val owner: Any? = null) : drodobyte.core.rx.Rx(), OnRx {
 
-    fun <T> onRender(`in`: In<T>, item: (T) -> Unit) = `in`.render(item)
-    override fun <T> In<T>.render(item: (T) -> Unit) = subscribeToSched(item, mainThread())
+    fun <T> onRender(`in`: () -> In<T>) = subs(`in`, mainThread())
+    override fun <T> In<T>.render(item: (T) -> Unit) = subs(item, mainThread())
 
     override fun log(it: Throwable) {
         Log.e("Rx", "subscription error, owner:${owner ?: this}", it)
