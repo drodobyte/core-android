5f5a7cc6e67f503194911ce8ff62af81b9661d0a
drodobyte <drodobyte@gmail.com>
2024-07-12 19:56:09 +0300

    warning

--- core/src/main/java/drodobyte/android/context/BaseFragment.kt
+++ core/src/main/java/drodobyte/android/context/BaseFragment.kt
@@ -1,7 +1,11 @@
 package drodobyte.android.context
 
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import io.reactivex.Maybe
