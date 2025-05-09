ade86dcc93d77d5c95ac2d79bc8e3a465a703da9
drodobyte <drodobyte@gmail.com>
2025-05-09 13:22:50 +0300

    new mvi

--- core/src/main/java/drodobyte/android/compose/Mvi.kt
+++ core/src/main/java/drodobyte/android/compose/Mvi.kt
@@ -5,8 +5,10 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.produceState
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import drodobyte.core.rx.Dispose
 import drodobyte.core.rx.In
 
 @Composable
@@ -40,4 +42,51 @@ fun <S : Any> mvi(
                 }
             }
     }
+}
+
+@Composable
+fun <S : Any> Mvi(
+    start: S,
+    states: In<S>,
+    reduce: (prev: S, now: S) -> S,
+    save: (S) -> S = { it },
+    content: @Composable (S) -> Unit
+) {
+    var saved by rememberSaveable { mutableStateOf(start) }
+    var state by remember { mutableStateOf(null as S?) }
+    val mvi = remember { MyMvi(saved, states, reduce) { state = it } }
+    produceState(null as S?) {
+        value = state
+        awaitDispose {
+            saved = save(value!!) // save state for recreation
+            mvi.dispose()
+        }
+    }
+    if (state != null) {
+        content(state!!)
+    }
+}
+
+private class MyMvi<S : Any>(
+    start: S,
+    states: In<S>,
+    reduce: (prev: S, now: S) -> S,
+    next: (S) -> Unit
+) : Dispose {
+    private var time = 0
+    private val d =
+        states
+            .startWithArray(start, start)
+            .scan { prev, now ->
+                reduce(prev, now)
+                    .also {
+                        if (time > 1)
+                            log("Compose-Mvi", "Reduce: [$now] + [$prev]  ->  [$it]")
+                    }
+            }
+            .distinctUntilChanged()
+            .doOnNext { log("Compose-Mvi", "Next  : [$it]") }
+            .subscribe { time++; next(it) }
+
+    override fun dispose() = d.dispose()
 }
